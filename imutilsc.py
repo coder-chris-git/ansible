90ed83842bde0fcc46aee05b375ff8aa0f5804e8
coder-chris-git <cdav49719@gmail.com>
2022-03-19 19:43:39 +0000

    my module

---
+++ imutilsc.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import subprocess
+from ansible.module_utils.basic import *
+
+
+def main():
+    module = \
+        AnsibleModule(argument_spec=dict(username=dict(required=True),
+                      password=dict(required=True),
+                      url=dict(default='http://www.ibm.com/software/repositorymanaer/entitled/repository.xml'
+                      , required=False, aliases=['repo']),
+                      source=dict(default='/opt/IBM/InstallationManager/eclipse/tools/imutilsc'
+                      , required=False)))
+    username = module.params['username']
+    password = module.params['password']
+    url = module.params['url']
+    source = module.params['source']
+
+    # Read arguments
+
+    child = subprocess.Popen([source + ' saveCredential -url ' + url
+                             + ' -userName ' + username
+                             + ' -userPassword ' + password],
+                             shell=True, stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+    stdout_value, stderr_value = child.communicate()
+    # Read arguments
+
+    if child.returncode != 0:
+        module.fail_json(msg="failed",stderr=stderr_value, stdout=stdout_value)
+
+                # Module finished
+
+    module.exit_json(changed=True,msg=username,stdout=stdout_value)
+
+
+if __name__ == '__main__':
+
+    main()
+

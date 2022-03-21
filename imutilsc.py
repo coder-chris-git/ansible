5eff41a90bb949aafa1dee3f353d2d7137ace63a
Christopher Davis <65687649+coder-chris-git@users.noreply.github.com>
2022-03-21 17:17:53 -0400

    Update imutilsc.py

--- imutilsc.py
+++ imutilsc.py
@@ -1,43 +1,92 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
-import os
 import subprocess
+import os
 from ansible.module_utils.basic import *
 
 
-def main():
-    module = \
-        AnsibleModule(argument_spec=dict(username=dict(required=True),
-                      password=dict(required=True),
-                      url=dict(default='http://www.ibm.com/software/repositorymanaer/entitled/repository.xml'
-                      , required=False, aliases=['repo']),
-                      source=dict(default='/opt/IBM/InstallationManager/eclipse/tools/imutilsc'
-                      , required=False)))
-    username = module.params['username']
-    password = module.params['password']
-    url = module.params['url']
-    source = module.params['source']
+class imutilsc():
+    module = None,
+    module_facts = dict(username=None, stdout=None, stderr=None)
 
-    # Read arguments
+    def __init__(self):
+        source_default = "/opt/IBM/InstallationManager/eclipse/tools/imutilsc"
 
-    child = subprocess.Popen([source + ' saveCredential -url ' + url
-                             + ' -userName ' + username
-                             + ' -userPassword ' + password],
-                             shell=True, stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE)
-    stdout_value, stderr_value = child.communicate()
-    # Read arguments
+        # create Dictionaries
 
-    if child.returncode != 0:
-        module.fail_json(msg="failed",stderr=stderr_value, stdout=stdout_value)
+        encryptString = dict(type='dict', options=dict(
+                str = dict(required = True),
+                source = dict(default=source_default)))
+        get = dict(type='str')
+        saveCredentials = dict(type='dict',options=dict(
+                username=dict(required=True),
+                password=dict(required=True),
+                url=dict(default = 'http://www.ibm.com/software/repositorymanager/entitled/repository.xml'),
+                source=dict(default=source_default)))
+#       set dictiornaires to Ansible agruement spec
+        self.module = AnsibleModule(argument_spec=dict(
+            #add dict to list
+            saveCredentials=dict(**saveCredentials),
+            encryptString =dict(**encryptString),
+            get=dict(**get)
 
-                # Module finished
+        ))
 
-    module.exit_json(changed=True,msg=username,stdout=stdout_value)
+    #functions using module params
+    def subpro(self,cmd,msg):
+        child = subprocess.Popen([
+            cmd
+        ],
+                                 shell=True,
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE)
+        stdout_value, stderr_value = child.communicate()
+        # Read arguments
+
+        if child.returncode != 0:
+            self.module.fail_json(msg="failed",
+                                   stderr=stderr_value,
+                                   stdout=stdout_value)
+        self.module.exit_json(changed=True, stdout=stdout_value)
+    def encryptString(self,module_params):
+        encryptString = module_params.get('encryptString')
+        source = encryptString['source']
+        str = encryptString['str']
+        cmd = source + " encryptString " + str
+        msg = str
+        self.subpro(cmd,msg)
 
 
+
+    def saveCredentials(self, module_params):
+        saveCredentials = module_params.get('saveCredentials')
+
+        username = saveCredentials['username']
+        password = saveCredentials['password']
+        url = saveCredentials['url']
+        source = saveCredentials['source']
+
+        cmd =  source + ' saveCredential -url ' + url + ' -userName ' + username + ' -userPassword ' + password
+        msg = saveCredentials
+
+        self.subpro(cmd,msg)
+
+    def get(self,module_params):
+        name = module_params['get']
+        source = "/opt/IBM/InstallationManager/eclipse/tools/imutilsc"
+
+
+        cmd = source + " " +  name
+        msg =  "uddsd"
+        self.subpro(cmd,msg)
+
+    def main(self):
+        if self.module.params['saveCredentials']:
+            self.saveCredentials(self.module.params)
+        if self.module.params['encryptString']:
+            self.encryptString(self.module.params)
+        if self.module.params['get']:
+            self.get(self.module.params)
 if __name__ == '__main__':
-
-    main()
-
+    imutilsc = imutilsc()
+    imutilsc.main()

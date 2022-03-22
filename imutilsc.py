dc568d1e43cda865482b6fc9406082c401b23ab8
Christopher Davis <65687649+coder-chris-git@users.noreply.github.com>
2022-03-21 21:05:09 -0400

    Update imutilsc.py

--- imutilsc.py
+++ imutilsc.py
@@ -9,49 +9,47 @@ class imutilsc():
 
     def __init__(self):
 
-        # set Dict to variable 
-        
+        # create Dictionaries usually when the dict is lengthy or nested
+
         saveCredential = dict(type='dict',options=dict(
                 username=dict(required=True),
                 password=dict(required=True),
                 url=dict(default = 'http://www.ibm.com/software/repositorymanager/entitled/repository.xml'),
         ))
 
-        # add dict to Ansible agruement spec by variables created above or by adding them directly.
+        # set dictiornaires to Ansible agruement spec  or add directly
 
         self.module = AnsibleModule(argument_spec=dict(
 
-            #add dict to arguement spec from var
+            #add dict to list from var
 
             saveCredential=dict(**saveCredential),
 
             #add directly
 
             encryptString =dict(type='str'),
-            get=dict(type='str'),
+            get=dict(type='str',choices=['help','version']),
             source=dict(type='str', default="/opt/IBM/InstallationManager/eclipse/tools/imutilsc")
         ))
 
-    #subpro function exists the module with json response 
+    #subpro function exits module with json response
 
     def subpro(self,cmd,msg):
         child = subprocess.Popen([
             cmd
         ],
-            shell=True  
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE)
-            stdout_value, stderr_value = child.communicate()
+                                 shell=True,
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE)
+        stdout_value, stderr_value = child.communicate()
         # Read arguments
 
         if child.returncode != 0:
             self.module.fail_json(msg="failed",
-            stderr=stderr_value,
-            stdout=stdout_value)
+                                   stderr=stderr_value,
+                                   stdout=stdout_value)
         self.module.exit_json(changed=True, stdout=stdout_value)
 
-    # imutilsc encrypt string command
-   
     def encryptString(self,module_params):
         encryptString = module_params['encryptString']
         source = module_params['source']
@@ -60,8 +58,7 @@ class imutilsc():
         self.subpro(cmd,msg)
 
 
-    # imutilsc save credential command
-    
+
     def saveCredential(self, module_params):
         saveCredential = module_params.get('saveCredential')
 
@@ -74,9 +71,7 @@ class imutilsc():
         msg = saveCredential
 
         self.subpro(cmd,msg)
-       
-    # command used call help and or version command 
-   
+
     def get(self,module_params):
         name = module_params['get']
         source = module_params['source']
@@ -86,8 +81,6 @@ class imutilsc():
         msg =  "uddsd"
         self.subpro(cmd,msg)
 
-    # call function based on user parameters
-    
     def main(self):
         if self.module.params['saveCredential']:
             self.saveCredential(self.module.params)
@@ -95,7 +88,6 @@ class imutilsc():
             self.encryptString(self.module.params)
         if self.module.params['get']:
             self.get(self.module.params)
-            
 if __name__ == '__main__':
     imutilsc = imutilsc()
     imutilsc.main()

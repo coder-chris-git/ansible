42042e0f9a0bd5774c4cb2b8d5dca0505736ae50
Christopher Davis <65687649+coder-chris-git@users.noreply.github.com>
2022-05-20 07:28:21 -0400

    Update custom_df.py

--- custom_df.py
+++ custom_df.py
@@ -3,61 +3,30 @@
 
 from ansible.module_utils.basic import *
 
-class custom_df():
-    
+class custom_df:
 
     def __init__(self):
- 
-        # create Dictionaries usually when the dict is lengthy or nested
-
-
-        # set dictiornaires to Ansible agruement spec  or add directly
-
-        self.module = AnsibleModule(argument_spec=dict(
-
-            #add dict to list from var
-        
-           
-                option = dict(),
-                dest = dict()
-
-
-        ))
-
-
-
-
-
-
+        self.module = AnsibleModule(argument_spec=dict(option=dict(),
+                                    dest=dict()))
 
     def main(self):
+
         df = self.module.params
         option = df['option']
         dest = df['dest']
+        cmd = 'df'
         
-        cmd = "df > {0}/df.out".format(dest)
+        if option and option == 'human' or 'hr' or 'human_readable':
+            cmd = cmd + ' -h'
+        child = subprocess.Popen([cmd], shell=True,
+                                 stdout=subprocess.PIPE)
+        (stdout_value, stderr_value) = child.communicate()
 
-        if option and option =='human':
-             cmd = "-h >".join(cmd.split(">"))
-
-
-        child = subprocess.Popen([
-            cmd
-        ],
-                                 shell=True,
-                                 stdout=subprocess.PIPE,
-                                 stderr=subprocess.PIPE)
-        stdout_value, stderr_value = child.communicate()
-        # Read arguments
-
-        if child.returncode != 0:
-            self.module.fail_json(msg="failed",
-                                   stderr=stderr_value,
-                                   stdout=stdout_value)
+        path = str(dest) + '/df.out'
+        with open(path, 'w') as log:
+            print >> log, stdout_value
         self.module.exit_json(changed=True, stdout=stdout_value)
 
-
-    
 
 if __name__ == '__main__':
     run_cmd = custom_df()
